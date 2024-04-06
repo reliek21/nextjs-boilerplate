a4241505130226d4a2ec5f497fe2c8ca27d197c8
Keiler Guardo Herrera <keilerguardo@gmail.com>
2024-04-05 23:39:14 -0500

    fix: git ignore

--- src/config/auth/auth.config.ts
+++ src/config/auth/auth.config.ts
@@ -7,6 +7,10 @@ export const authOptions = {
 	secret: CONFIG_ENVIRONMENTS.NEXT_AUTH_SECRET,
 	providers: [credentialsProviderConfig],
 	pages: {
-		signIn: AUTH_ROUTES.login
+		signIn: AUTH_ROUTES.login,
+		signOut: '/auth/signout',
+		error: '/auth/error', // Error code passed in query string as ?error=
+		verifyRequest: '/auth/verify-request', // (used for check email message)
+		newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
 	}
 };
